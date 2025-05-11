701bea3490078ef8d5eee88e1ae80f35859db03a
alexforrester <aforresterwork@gmail.com>
2025-05-11 19:43:48 +0100

    Updates Chapter 2 exercises

--- Chapter02/Exercise02.02/app/src/main/java/com/example/saveandrestore/MainActivity.kt
+++ Chapter02/Exercise02.02/app/src/main/java/com/example/saveandrestore/MainActivity.kt
@@ -4,37 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.saveandrestore.ui.theme.SaveAndRestoreTheme
+
+import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.saveandrestore.ui.theme.SaveAndRestoreTheme
 import kotlin.random.Random
-
 
 class MainActivity : ComponentActivity() {
 
+
     private var randomNumber by mutableStateOf(0)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         if (savedInstanceState != null) {
-            randomNumber = savedInstanceState.getInt(RANDOM_NUMBER, 0)
+            randomNumber =
+                savedInstanceState.getInt(RANDOM_NUMBER, 0)
         }
+
 
         setContent {
             SaveAndRestoreTheme {
@@ -46,24 +52,30 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                             .fillMaxWidth(),
                     ) {
-                        Button(onClick = { randomNumber = generateRandomNumber() }) {
+                        Button(
+                            onClick = {
+                                randomNumber = generateRandomNumber()
+                            }
+                        ) {
                             Text(
-                                stringResource(id = R.string.generate_random_number),
+                                stringResource(
+                                    id = R.string.generate_random_number
+                                ),
                                 fontSize = 18.sp
                             )
                         }
                         Text(
-                            stringResource(id = R.string.random_number_message, randomNumber),
+                            stringResource(
+                                id = R.string.random_number_message,
+                                randomNumber
+                            ),
                             fontSize = 18.sp
                         )
                     }
+
                 }
             }
         }
-    }
-
-    private fun generateRandomNumber(): Int {
-        return Random.nextInt(0, 1000)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -74,4 +86,27 @@ class MainActivity : ComponentActivity() {
     companion object {
         private const val RANDOM_NUMBER = "RANDOM_NUMBER"
     }
+
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SaveAndRestoreTheme {
+        Greeting("Android")
+    }
+}
+
+private fun generateRandomNumber(): Int {
+    return Random.nextInt(0, 1000)
 }
--- Chapter02/Exercise02.03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter02/Exercise02.03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,20 @@ import android.view.Gravity
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,93 +26,113 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.runtime.saveable.rememberSaveable
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                MainScreen()
-            }
-        }
-    }
-}
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-@Composable
-private fun MainScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        var firstName by rememberSaveable { mutableStateOf("") }
-        var lastName by rememberSaveable { mutableStateOf("") }
-        var fullName by rememberSaveable { mutableStateOf("") }
+                    var firstName by rememberSaveable { mutableStateOf("") }
+                    var lastName by rememberSaveable { mutableStateOf("") }
+                    var fullName by rememberSaveable { mutableStateOf("") }
 
-        val welcomeMessage = stringResource(id = R.string.welcome_to_the_app)
-        val enterNameErrorMessage = stringResource(id = R.string.please_enter_a_name)
-        val context = LocalContext.current
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = firstName,
-                onValueChange = { firstName = it },
-                label = { Text(text = stringResource(id = R.string.first_name)) },
-            )
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = lastName,
-                onValueChange = { lastName = it },
-                label = { Text(text = stringResource(id = R.string.last_name)) }
-            )
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    if (firstName.isNotBlank() && lastName.isNotBlank())
-                        fullName = "$firstName $lastName"
-                    else {
-                        Toast.makeText(
-                            context,
-                            enterNameErrorMessage,
-                            Toast.LENGTH_LONG
+                    val welcomeMessage = stringResource(id = R.string.welcome_to_the_app)
+                    val enterNameErrorMessage = stringResource(id = R.string.please_enter_a_name)
+
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(16.dp),
+                        horizontalAlignment =
+                            Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+
+
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = firstName,
+                            onValueChange = { firstName = it },
+                            label = {
+                                Text(
+                                    text = stringResource(
+                                        id = R.string.first_name
+                                    )
+                                )
+                            },
                         )
-                            .apply {
-                                setGravity(Gravity.CENTER, 0, 0)
-                                show()
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = lastName,
+                            onValueChange = { lastName = it },
+                            label = {
+                                Text(
+                                    text = stringResource(
+                                        id = R.string.last_name
+                                    )
+                                )
                             }
-                        fullName = ""
+                        )
+
+
+                        val context = LocalContext.current
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                if (
+                                    firstName.isNotBlank() &&
+                                    lastName.isNotBlank())
+                                    fullName = "$firstName $lastName"
+                                else {
+                                    fullName = ""
+                                    val toast = Toast.makeText(
+                                        context,
+                                        enterNameErrorMessage,
+                                        Toast.LENGTH_LONG
+                                    )
+                                    toast.setGravity(Gravity.CENTER, 0, 0)
+                                    toast.show()
+                                }
+                            }
+                        ) {
+                            Text("Enter")
+                        }
+                        if (fullName.isNotBlank()) {
+                            Text(text = "$welcomeMessage $fullName!")
+                        }
+
+
                     }
+
                 }
-            ) {
-                Text("Enter")
-            }
-            if (fullName.isNotEmpty()) {
-                Text(text = "$welcomeMessage $fullName!")
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun MainScreenPreview() {
-    MainScreen()
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
 }
