4ce317e98bb0d2e7a22daae936d2ff43b9229b9b
alexforrester <aforresterwork@gmail.com>
2025-06-03 13:55:58 +0100

    Updates Activity 2.01 and 2.02

--- Chapter02/Activity02.01/app/src/main/java/com/example/loginform/LoginActivity.kt
+++ Chapter02/Activity02.01/app/src/main/java/com/example/loginform/LoginActivity.kt
@@ -3,29 +3,38 @@ package com.example.loginform
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.loginform.ui.theme.LoginFormTheme
 
-class LoginActivity : ComponentActivity() {
 
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val username = intent.getStringExtra(USERNAME_KEY)
-        val password = intent.getStringExtra(PASSWORD_KEY)
-
-
+        enableEdgeToEdge()
         setContent {
+
+            val username = intent.getStringExtra(USERNAME_KEY)
+            val password = intent.getStringExtra(PASSWORD_KEY)
+
             LoginFormTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val message = if (username == "username" && password == "password") {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) { innerPadding ->
+                    val message = if (
+                        username == "username" &&
+                        password == "password"
+                    ) {
                         "Welcome, $username!"
                     } else {
                         "Login failed. Please try again."
@@ -37,11 +46,31 @@ class LoginActivity : ComponentActivity() {
                             .padding(innerPadding),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text(text = message, modifier = Modifier.padding(16.dp))
+                        Text(
+                            text = message,
+                            modifier = Modifier.padding(16.dp)
+                        )
 
                     }
                 }
+
             }
         }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    LoginFormTheme {
+        Greeting2("Android")
     }
 }
