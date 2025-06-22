e4a3b0d484e8335db72e7dc62907f538831d0c29
alexforrester <aforresterwork@gmail.com>
2025-06-22 19:09:20 +0100

    Updates route navigation

--- Chapter04/Exercise04.03/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Chapter04/Exercise04.03/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.bottomnavigation
 
+import android.R.attr.label
+import android.R.attr.onClick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,25 +9,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.bottomnavigation.Route.Bin
-import com.example.bottomnavigation.Route.Calendar
-import com.example.bottomnavigation.Route.Favorites
-import com.example.bottomnavigation.Route.Home
-import com.example.bottomnavigation.Route.Shopping
+import androidx.navigation.toRoute
+import com.example.bottomnavigation.Destinations.Bin
+import com.example.bottomnavigation.Destinations.Calendar
+import com.example.bottomnavigation.Destinations.Favorites
+import com.example.bottomnavigation.Destinations.Home
+import com.example.bottomnavigation.Destinations.Shopping
 import com.example.bottomnavigation.ui.theme.BottomNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,38 +49,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainApp(navController: NavHostController = rememberNavController()) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
 
-    val routes = listOf(Home, Shopping, Favorites, Calendar, Bin)
+    val bottomNavigationItems = listOf(
+        BottomNavigation.Home,
+        BottomNavigation.Shopping,
+        BottomNavigation.Favorites,
+        BottomNavigation.Calendar,
+        BottomNavigation.Bin
+    )
 
     Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(stringResource(R.string.app_name)) })
+        },
         bottomBar = {
             NavigationBar {
-                routes.forEach { item ->
-                    val selected = currentDestination?.hasRoute(item::class) == true
+                bottomNavigationItems.forEach { item ->
+
+                    val selected = navBackStackEntry?.destination?.hasRoute(item.route::class) == true
+
                     NavigationBarItem(
                         selected = selected,
                         icon = {
-                            BadgedBox(
-                                badge = {
-                                    if (item.badgeCount > 0) {
-                                        Badge { Text(item.badgeCount.toString()) }
-                                    }
-                                }
-                            ) {
-                                Icon(
-                                    imageVector = if (selected) item.selectedIcon else item.unselectedIcon,
-                                    contentDescription = item.label
-                                )
-                            }
+                            Icon(
+                                imageVector = if (selected) item.selectedIcon else item.unselectedIcon,
+                                contentDescription = item.label
+                            )
                         },
                         label = { Text(item.label) },
                         onClick = {
                             if (!selected) {
-                                navController.navigate(item) {
+                                navController.navigate(item.route) {
                                     launchSingleTop = true
                                     restoreState = true
                                     popUpTo(navController.graph.startDestinationId) {
@@ -90,11 +102,11 @@ fun MainApp(navController: NavHostController = rememberNavController()) {
             modifier = Modifier.padding(innerPadding),
             startDestination = Home
         ) {
-            composable<Home> { Home.label }
-            composable<Shopping> { Shopping.label }
-            composable<Favorites> { Favorites.label }
-            composable<Calendar> { Calendar.label }
-            composable<Bin> { Bin.label }
+            composable<Home> { ContentScreen(Home.label) }
+            composable<Shopping> { ContentScreen(Shopping.label) }
+            composable<Favorites> { ContentScreen(Favorites.label) }
+            composable<Calendar> { ContentScreen(Calendar.label) }
+            composable<Bin> { ContentScreen( Bin.label) }
         }
     }
 }
