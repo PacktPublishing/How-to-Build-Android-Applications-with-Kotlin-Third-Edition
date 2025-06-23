683fb90509af8ac101dd374a31d517f87e951bf5
alexforrester <aforresterwork@gmail.com>
2025-06-23 18:00:38 +0100

    Upgrades example 1 gralde wrapper

--- Chapter04/Exercise04.02/app/src/main/java/com/example/navigationdrawer/ui/theme/Theme.kt
+++ Chapter04/Exercise04.02/app/src/main/java/com/example/navigationdrawer/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BottomNavigationTheme(
+fun NavigationDrawerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
