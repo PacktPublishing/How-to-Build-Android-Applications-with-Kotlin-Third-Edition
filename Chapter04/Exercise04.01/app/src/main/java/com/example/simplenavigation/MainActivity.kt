d9a70045bd663360cc7247d7e8e1aa4761721018
alexforrester <aforresterwork@gmail.com>
2025-06-23 18:56:36 +0100

    Converts Exercise 1 to use type safe navigation

--- Chapter04/Exercise04.01/app/src/main/java/com/example/simplenavigation/MainActivity.kt
+++ Chapter04/Exercise04.01/app/src/main/java/com/example/simplenavigation/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.simplenavigation.ui.theme.SimpleNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +26,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NavigationApp() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Routes.Home.route) {
-        composable(Routes.Home.route) { HomeScreen(navController) }
-        composable(Routes.Red.route) { ColorScreen(navController, "RED", Color.Red) }
-        composable(Routes.Green.route) { ColorScreen(navController, "GREEN", Color.Green) }
-        composable(Routes.Blue.route) { ColorScreen(navController, "BLUE", Color.Blue) }
+    NavHost(navController = navController, startDestination = Home) {
+        composable<Home> { HomeScreen(navController) }
+        composable<Color> { navBackStackEntry ->
+            val color = navBackStackEntry.toRoute<Color>()
+            ColorScreen(navController, color.name, color.value) }
     }
 }
 
