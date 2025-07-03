622c3a5985de6c0add0632bcc88c27c52647baa5
alexforrester <aforresterwork@gmail.com>
2025-07-03 07:57:11 +0100

    Changes to Chapter 4 exercise 03

--- Chapter04/Exercise04.03/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Chapter04/Exercise04.03/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -102,6 +102,7 @@ fun MainApp() {
                             if (!isSelected ) {
                                 navController.navigate(item.route) {
                                     launchSingleTop = true
+                                    restoreState = true
                                     popUpTo(navController.graph.startDestinationId) {
                                         saveState = true
                                     }
