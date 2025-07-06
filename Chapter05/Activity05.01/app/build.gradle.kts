dc91f8e0ad4bd3c969a67d68bcc8875cf1fbd2dc
Eran Boudjnah <eranbou@gmail.com>
2025-07-06 18:18:54 +0100

    Updated dependencies+SDK for chapters 5-8. Removed some dead code.

--- Chapter05/Activity05.01/app/build.gradle.kts
+++ Chapter05/Activity05.01/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -5,14 +7,20 @@ plugins {
     kotlin("plugin.serialization") version "2.1.0"
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
 android {
     namespace = "com.example.weatherapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.weatherapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -34,9 +42,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
