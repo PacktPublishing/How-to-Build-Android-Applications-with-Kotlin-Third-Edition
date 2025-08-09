9ac1037acb862efd8067ebb96292b5e3402bfe42
alexforrester <aforresterwork@gmail.com>
2025-08-09 11:10:05 +0100

    Updates Exercise 01.03 with correct package name

--- Chapter01/Exercise01.03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter01/Exercise01.03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,39 +1,41 @@
-package com.example.jetpackcompose.myapplication
+package com.example.myapplication
 
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import com.example.jetpackcompose.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             val webView = WebView(this)
             webView.settings.javaScriptEnabled = true
             webView.loadUrl("https://www.google.com")
-
             AndroidView(
                 modifier = Modifier.fillMaxSize(),
                 factory = { context ->
                     webView
-                })
+                }
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    name: String, modifier: Modifier = Modifier
+) {
     Text(
+        style = MaterialTheme.typography.displayLarge,
         text = "Hello $name!",
         modifier = modifier
     )
